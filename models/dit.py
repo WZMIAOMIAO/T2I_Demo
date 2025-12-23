9bc983975b4716f6ad5eeda19ba6d8af1a6a2d42
WuZhe <605169423@qq.com>
2025-12-24 00:16:55 +0800

    add rope

--- models/dit.py
+++ models/dit.py
@@ -8,20 +8,66 @@
 # GLIDE: https://github.com/openai/glide-text2im
 # MAE: https://github.com/facebookresearch/mae/blob/main/models_mae.py
 # --------------------------------------------------------
-from typing import Optional
+import math
+from typing import Optional, Tuple, List
+from functools import lru_cache
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-import math
 from timm.models.vision_transformer import PatchEmbed
+from diffusers.models.embeddings import apply_rotary_emb, get_1d_rotary_pos_embed
 
 
 def replicate(x: torch.Tensor, num: int = 1) -> torch.Tensor:
     x = x.unsqueeze(1)  # [B, H] -> [B, 1, H]
     x = x.repeat([1, num, 1])  # [B, S, H]
     return x
+
+
+@lru_cache(maxsize=8)
+def prepare_latent_image_ids(height, width):
+    """
+    copy from diffusers.pipelines.flux.pipeline_flux
+
+    :param height: latent height
+    :param width: latent width
+    """
+    latent_image_ids = torch.zeros(height, width, 3)
+    latent_image_ids[..., 1] = latent_image_ids[..., 1] + torch.arange(height)[:, None]
+    latent_image_ids[..., 2] = latent_image_ids[..., 2] + torch.arange(width)[None, :]
+
+    latent_image_id_height, latent_image_id_width, latent_image_id_channels = latent_image_ids.shape
+
+    latent_image_ids = latent_image_ids.reshape(
+        latent_image_id_height * latent_image_id_width, latent_image_id_channels
+    )
+
+    return latent_image_ids
+
+
+@lru_cache(maxsize=8)
+def prepare_cond_ids(seq_length: int):
+    """
+    refer diffusers.pipelines.flux.pipeline_flux
+
+    :param seq_length:
+    """
+    cond_ids = torch.zeros(seq_length, 3)
+
+    return cond_ids
+
+
+def create_axes_dim(dim: int, num_head: int) -> List[int]:
+    assert dim % num_head == 0
+    dim_per_head = dim // num_head
+
+    dim_t = 16
+    dim_h = dim_w = (dim_per_head - dim_t) // 2
+    assert dim_t + dim_h + dim_w == dim_per_head
+
+    return [dim_t, dim_h, dim_w]
 
 
 #################################################################################
@@ -104,6 +150,35 @@ class LabelEmbedder(nn.Module):
         return embeddings
 
 
+class FluxPosEmbed(nn.Module):
+    # modified from https://github.com/black-forest-labs/flux/blob/c00d7c60b085fce8058b9df845e036090873f2ce/src/flux/modules/layers.py#L11
+    def __init__(self, theta: int, axes_dim: List[int]):
+        super().__init__()
+        self.theta = theta
+        self.axes_dim = axes_dim
+
+    def forward(self, ids: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        n_axes = ids.shape[-1]
+        cos_out = []
+        sin_out = []
+        pos = ids.float()
+        freqs_dtype = torch.float32
+        for i in range(n_axes):
+            cos, sin = get_1d_rotary_pos_embed(
+                self.axes_dim[i],
+                pos[:, i],
+                theta=self.theta,
+                repeat_interleave_real=True,
+                use_real=True,
+                freqs_dtype=freqs_dtype,
+            )
+            cos_out.append(cos)
+            sin_out.append(sin)
+        freqs_cos = torch.cat(cos_out, dim=-1).to(ids.device)
+        freqs_sin = torch.cat(sin_out, dim=-1).to(ids.device)
+        return freqs_cos, freqs_sin
+
+
 #################################################################################
 #                                 Core DiT Model                                #
 #################################################################################
@@ -124,6 +199,7 @@ class Attention(nn.Module):
             qkv_bias: bool = True,
             attn_drop: float = 0.,
             proj_drop: float = 0.,
+            fused_attn: bool = True
     ) -> None:
         """Initialize the Attention module.
 
@@ -133,12 +209,14 @@ class Attention(nn.Module):
             qkv_bias: Whether to use bias in the query, key, value projections
             attn_drop: Dropout rate applied to the attention weights
             proj_drop: Dropout rate applied after the output projection
+            fused_attn: Whether to use fused attention
         """
         super().__init__()
         assert dim % num_heads == 0, 'dim should be divisible by num_heads'
         self.num_heads = num_heads
         self.head_dim = dim // num_heads
         self.scale = self.head_dim ** -0.5
+        self.fused_attn = fused_attn
 
         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
         self.q_norm = nn.RMSNorm(self.head_dim)
@@ -150,6 +228,7 @@ class Attention(nn.Module):
     def forward(
             self,
             x: torch.Tensor,
+            rotary_emb: Tuple[torch.Tensor],
             attn_mask: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
         B, N, C = x.shape
@@ -157,12 +236,24 @@ class Attention(nn.Module):
         q, k, v = qkv.unbind(0)
         q, k = self.q_norm(q), self.k_norm(k)
 
-        x = F.scaled_dot_product_attention(
-            q, k, v,
-            attn_mask=attn_mask,
-            scale=self.scale,
-            dropout_p=self.attn_drop.p if self.training else 0.,
-        )
+        q = apply_rotary_emb(q, rotary_emb, sequence_dim=2)
+        k = apply_rotary_emb(k, rotary_emb, sequence_dim=2)
+
+        if self.fused_attn:
+            x = F.scaled_dot_product_attention(
+                q, k, v,
+                attn_mask=attn_mask,
+                scale=self.scale,
+                dropout_p=self.attn_drop.p if self.training else 0.,
+            )
+        else:
+            q = q * self.scale
+            attn = q @ k.transpose(-2, -1)
+            if attn_mask:
+                attn = attn + attn_mask
+            attn = attn.softmax(dim=-1)
+            attn = self.attn_drop(attn)
+            x = attn @ v
 
         x = x.transpose(1, 2).reshape(B, N, C)
         x = self.proj(x)
@@ -206,8 +297,8 @@ class DiTBlock(nn.Module):
         mlp_hidden_dim = int(hidden_size * mlp_ratio * 2 / 3)
         self.mlp = SwiGLUFFN(in_features=hidden_size, hidden_features=mlp_hidden_dim)
 
-    def forward(self, x):
-        x = x + self.attn(self.norm1(x))
+    def forward(self, x, rotary_emb):
+        x = x + self.attn(self.norm1(x), rotary_emb)
         x = x + self.mlp(self.norm2(x))
         return x
 
@@ -246,29 +337,31 @@ class DiT(nn.Module):
         num_heads=16,
         mlp_ratio=4.0,
         class_dropout_prob=0.1,
-        num_classes=1000
+        num_classes=1000,
+        fused_attn=True
     ):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = in_channels
         self.patch_size = patch_size
         self.num_heads = num_heads
+        self.axes_dim = create_axes_dim(hidden_size, num_heads)
 
         self.x_embedder = PatchEmbed(input_size, patch_size, in_channels, hidden_size, bias=True)
         self.t_embedder = TimestepEmbedder(hidden_size)
         self.h_embedder = TimestepEmbedder(hidden_size)
         self.w_embedder = TimestepEmbedder(hidden_size)
-        self.t_min_embedder = TimestepEmbedder(hidden_size)
-        self.t_max_embedder = TimestepEmbedder(hidden_size)
+        self.interval_embedder = TimestepEmbedder(hidden_size)
         self.c_embedder = LabelEmbedder(num_classes, hidden_size, class_dropout_prob)
         cond_seq = 4 + 4 + 4 + 2 + 2 + 8
-        self.pos_embed = nn.Parameter(torch.zeros(1, cond_seq, hidden_size), requires_grad=True)
+        self.learnable_embed = nn.Parameter(torch.zeros(1, cond_seq, hidden_size), requires_grad=True)
+        self.pos_embed = FluxPosEmbed(1000, self.axes_dim)
 
         self.blocks = nn.ModuleList(
-            [DiTBlock(hidden_size, num_heads, mlp_ratio=mlp_ratio) for _ in range(depth)]
+            [DiTBlock(hidden_size, num_heads, mlp_ratio=mlp_ratio, fused_attn=fused_attn) for _ in range(depth)]
         )
         self.final_layer = FinalLayer(hidden_size, patch_size, self.out_channels)
-        self.initialize_weights()
+        # self.initialize_weights()
 
     def initialize_weights(self):
         # Initialize transformer layers:
@@ -321,7 +414,7 @@ class DiT(nn.Module):
         imgs = x.reshape(shape=(x.shape[0], c, h * p, h * p))
         return imgs
 
-    def forward(self, x, t, h, w, t_min, t_max, c):
+    def forward(self, x, t, h, w, t_min, t_max, c, img_ids, cond_ids):
         """
         Forward pass of DiT.
         x: (N, C, H, W) tensor of spatial inputs (images or latent representations of images)
@@ -330,12 +423,12 @@ class DiT(nn.Module):
         """
         x_tokens = self.x_embedder(x)             # (N, T, D), where T = H * W / patch_size ** 2
         x_seq = x_tokens.shape[1]
-        t_embed = self.t_embedder(t)              # (N, D)
-        h_embed = self.h_embedder(h)              # (N, D)
-        w_embed = self.w_embedder(w)              # (N, D)
-        t_min_embed = self.t_min_embedder(t_min)  # (N, D)
-        t_max_embed = self.t_max_embedder(t_max)  # (N, D)
-        c_embed = self.c_embedder(c)              # (N, D)
+        t_embed = self.t_embedder(t)                 # (N, D)
+        h_embed = self.h_embedder(h)                 # (N, D)
+        w_embed = self.w_embedder(w)                 # (N, D)
+        t_min_embed = self.interval_embedder(t_min)  # (N, D)
+        t_max_embed = self.interval_embedder(t_max)  # (N, D)
+        c_embed = self.c_embedder(c)                 # (N, D)
         cond_for_final_layer = t_embed + h_embed + w_embed + t_min_embed + t_max_embed + c_embed
 
         c_tokens = torch.concat(
@@ -349,13 +442,16 @@ class DiT(nn.Module):
             ],
             dim=1
         )
-        c_tokens = c_tokens + self.pos_embed
-        x = torch.concat([x_tokens, c_tokens], dim=1)
+        c_tokens = c_tokens + self.learnable_embed
+        x = torch.concat([c_tokens, x_tokens], dim=1)
+
+        ids = torch.cat((cond_ids, img_ids), dim=0)
+        rotary_emb = self.pos_embed(ids)
 
         for block in self.blocks:
-            x = block(x)                                # (N, T, D)
+            x = block(x, rotary_emb)                    # (N, T, D)
 
-        x = x[:, :x_seq]
+        x = x[:, -x_seq:]
         x = self.final_layer(x, cond_for_final_layer)   # (N, T, patch_size ** 2 * out_channels)
         x = self.unpatchify(x)                          # (N, out_channels, H, W)
         return x
@@ -430,3 +526,22 @@ def iMF_L(**kwargs) -> DiT:
 
 def iMF_B(**kwargs) -> DiT:
     return DiT(depth=12, hidden_size=768, patch_size=2, num_heads=12, **kwargs)
+
+
+if __name__ == '__main__':
+    dit = iMF_B()
+    params = sum(p.numel() for p in dit.parameters())
+    print(f"params: {params}")
+
+    x = torch.randn(size=(1, 4, 32, 32))
+    t = torch.rand(size=(1,))
+    h = torch.rand(size=(1,))
+    w = torch.rand(size=(1,))
+    t_min = torch.rand(size=(1,))
+    t_max = torch.rand(size=(1,))
+    c = torch.randint(low=0, high=999, size=(1,))
+    img_ids = prepare_latent_image_ids(16, 16)
+    cond_ids = prepare_cond_ids(24)
+
+    res = dit(x, t, h, w, t_min, t_max, c, img_ids, cond_ids)
+    print(f"res shape: {res.shape}")
